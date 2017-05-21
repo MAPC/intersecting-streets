b83b50cf55a311cf42124c79d8beeb391a84c8d9
allthesignals <wilburnforce@gmail.com>
2017-05-21 17:28:07 -0400

    Create childsurveys wrapper component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import StreetDropdown from './StreetDropdown';
+import ChildSurveys from './ChildSurveys';
 
 import './App.css';
 
@@ -8,6 +9,7 @@ class App extends Component {
     return (
       <div className="App">
         <StreetDropdown />
+        <ChildSurveys />
       </div>
     );
   }
