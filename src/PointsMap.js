bbe09c0b4f384a9810a149a11ffb5ad2858f5f01
allthesignals <wilburnforce@gmail.com>
2017-05-24 14:29:15 -0400

    Remove console log; add school marker

--- src/PointsMap.js
+++ src/PointsMap.js
@@ -7,13 +7,13 @@ class PointsMap extends Component  {
   }
 
   render() {
-    console.log(this.props.customPoint);
     return (
       <Map center={this.props.center} zoom={this.props.zoom} onClick={this.props.addCustomPoint.bind(this)}>
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
         />
+        <Marker position={this.props.school} />
         {
           this.props.points.map((point, index) => 
             <Marker onClick={this.props.onMarkerClick.bind(this, index)} position={[point.lat, point.lng]} key={index} />
